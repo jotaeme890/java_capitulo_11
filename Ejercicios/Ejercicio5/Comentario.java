fac9e4a7dde47599a1b65239ecc4872763ccd33a
jotaeme890 <permanentjavi@gmail.com>
2023-02-05 18:28:08 +0100

    Comentario

---
+++ Ejercicios/Ejercicio5/Comentario.java
@@ -0,0 +1,15 @@
+package Ejercicios.Ejercicio5;
+
+public class Comentario {
+    public static void main(String[] args) {
+        //ESTO ES UN COMENTARIO DE LÍNEA
+        /*ESTO 
+         * ES 
+         * UN 
+         * COMENTARIO 
+         * DE 
+         * BLOQUE
+         */
+        System.out.println("Hola");
+    }
+}
